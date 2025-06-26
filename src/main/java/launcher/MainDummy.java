4f235a18df161bf54cdd1556936abe45708a01e6
dmonarchc <cisternasdelriodaniel@gmail.com>
2025-06-26 01:48:13 -0400

    Add some structure to the project and gradle

---
+++ src/main/java/launcher/MainDummy.java
@@ -0,0 +1,8 @@
+package launcher;
+
+public class MainDummy {
+    public static void main(String[] args) {
+        System.out.println("This is a dummy main class for Gradle configuration.");
+        System.out.println("Use: ./gradlew run -Pmain=chapterXX.YourClass");
+    }
+}
